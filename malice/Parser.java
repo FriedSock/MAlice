f3b0227f26467d81f59e8a13fbd829331d5e96e6
Milan Misak <misakmilan@gmail.com>
2011-11-18 23:58:25 +0000

    Variable type checking and so

--- malice/Parser.java
+++ malice/Parser.java
@@ -7,24 +7,28 @@ import malice.commands.VariableDeclarationCommand;
 import malice.commands.VariableAssignmentCommand;
 import malice.commands.Command;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.antlr.runtime.tree.Tree;
 
 public class Parser {
 
     private List<Command> commands;
+    private Map<String, Symbol> symbolTable;
 
     public Parser() {
         commands = new ArrayList<Command>();
+        symbolTable = new HashMap<String, Symbol>();
     }
 
+    @Override
     public String toString() {
         StringBuilder b = new StringBuilder();
         for (Command command : commands) {
             b.append(command);
             b.append('\n');
         }
-        
         return b.toString();
     }
     
@@ -62,6 +66,12 @@ public class Parser {
     private Command parseVariableDeclaration(Tree tree) {
         String variableName = tree.getChild(0).getText();
         Type variableType = Type.valueOf(tree.getChild(2).getText());
+        
+        if (symbolTable.containsKey(variableName)) {
+            throw new VariableAlreadyDeclaredException(variableName);
+        }
+        
+        symbolTable.put(variableName, new Symbol(variableType));
 
         return new VariableDeclarationCommand(variableName, variableType);
     }
@@ -74,9 +84,18 @@ public class Parser {
 
         if ('\'' == expressionText.charAt(0)) {
             // character expression
+            Type variableType = symbolTable.get(variableName).getType();
+            if (Type.letter != variableType) {
+                throw new IncompatibleTypeException(variableName, variableType);
+            }
+
             expression = new CharacterExpression(expressionText.charAt(1));
         } else {
             // arithmetic expression
+            Type variableType = symbolTable.get(variableName).getType();
+            if (Type.number != variableType) {
+                throw new IncompatibleTypeException(variableName, variableType);
+            }
 
             //TODO
 
@@ -99,6 +118,18 @@ public class Parser {
         } else {
             // in case of syntax connectors such as and
             return null;
+        }
+    }
+    
+    public static class VariableAlreadyDeclaredException extends RuntimeException {
+        public VariableAlreadyDeclaredException(String variableName) {
+            super(variableName + " was already declared");
+        }
+    }
+    
+    public static class IncompatibleTypeException extends RuntimeException {
+        public IncompatibleTypeException(String variableName, Type type) {
+            super(variableName + " can only be assigned a value of type " + type);
         }
     }
 }
