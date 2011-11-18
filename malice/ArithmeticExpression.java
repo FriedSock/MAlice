cda3b9f03668012caff7e708416eae6f62fb2a56
Jack Bracewell <jbtwentythree@gmail.com>
2011-11-18 21:51:22 +0000

    I did not

--- malice/ArithmeticExpression.java
+++ malice/ArithmeticExpression.java
@@ -1,35 +1,36 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package malice;
 
-/**
- *
- * @author jack
- */
 public class ArithmeticExpression implements Expression {
+
     private ArithmeticExpression left;
     private ArithmeticExpression right;
     private char binOp;
-    private int value = 0;
+    private int value;
+    private String variable;
+    private boolean valueHasBeenSet;
 
     public ArithmeticExpression(int v){
         value = v;
+        valueHasBeenSet = true;
+    }
+
+    public ArithmeticExpression(String variable){
+        this.variable = variable;
     }
 
     //Don't need to pass in operator as the only unary operator is ~
     public ArithmeticExpression(ArithmeticExpression n){
         left = n;
     }
+
     public ArithmeticExpression(ArithmeticExpression left, ArithmeticExpression right, char op){
         this.left = left;
         this.right = right;
         binOp = op;
     }
-
-
-
-
+    
+    //TODO Optimisation - Collapse arithmetic parse tree
+    public void reduce() {   
+    }
+    
 }
