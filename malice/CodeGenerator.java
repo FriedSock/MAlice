1f488111b41b2b3e71d2473d43414bfc606a36b8
Milan Misak <misakmilan@gmail.com>
2011-11-19 18:06:31 +0000

    Added SymbolTable and CodeGenerator

---
+++ malice/CodeGenerator.java
@@ -0,0 +1,21 @@
+package malice;
+
+import java.util.ArrayList;
+import java.util.List;
+import malice.commands.Command;
+
+public class CodeGenerator {
+    
+    private List<Command> command;
+    private SymbolTable symbolTable;
+    
+    public CodeGenerator(List<Command> command, SymbolTable symbolTable) {
+        
+    }
+    
+    public List<String> generateCode() {
+        
+        
+        return new ArrayList<String>();
+    }
+}
---
+++ malice/SymbolTable.java
@@ -0,0 +1,23 @@
+package malice;
+
+import java.util.Map;
+
+public class SymbolTable {
+    
+    private Map<String, Symbol> symbolTable;
+    
+    public SymbolTable() {
+    }
+    
+    public void add(String variableName, Type variableType) {
+        symbolTable.put(variableName, new Symbol(variableType));
+    }
+    
+    public boolean containsVariable(String variableName) {
+        return symbolTable.containsKey(variableName);
+    }
+    
+    public Type getVariableType(String variableName) {
+        return symbolTable.get(variableName).getType();
+    }
+}
