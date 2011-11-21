c99ae0f31e374b9079406ad664fc44464d2361ff
Milan Misak <misakmilan@gmail.com>
2011-11-21 21:45:18 +0000

    Git

--- malice/MAlice.java
+++ malice/MAlice.java
@@ -14,7 +14,7 @@ import org.antlr.runtime.debug.ParseTreeBuilder;
 public class MAlice {
 
     public static void main(String[] args) {
-        /*if (args.length == 0) {
+        if (args.length == 0) {
             die("Please provide a file name of an Alice file as an argument to this program");
         }
 
@@ -31,8 +31,7 @@ public class MAlice {
             die("Build", ex);
         }
 
-        MAliceLexer lexer = new MAliceLexer(fileStream);*/
-        MAliceLexer lexer = new MAliceLexer(new ANTLRStringStream("i was a number, i became 22. j was a number and j became 13. j became i & j. j spoke."));
+        MAliceLexer lexer = new MAliceLexer(fileStream);
         CommonTokenStream tokenStream = new CommonTokenStream(lexer);
         ParseTreeBuilder builder = new ParseTreeBuilder("prog");
         MAliceParser maliceParser = new MAliceParser(tokenStream, builder);
@@ -52,11 +51,8 @@ public class MAlice {
         System.out.println("Generating assembly code");
         CodeGenerator codeGenerator = new CodeGenerator(parser.getCommands(), parser.getSymbolTable());
         List<String> assembly = codeGenerator.generateCode();
-        for (String asm : assembly) {
-            System.out.println(asm);
-        }
         
-        /*String baseFileName = maliceFileName.replaceAll(".alice$", "");
+        String baseFileName = maliceFileName.replaceAll(".alice$", "");
         String assemblyFileName = baseFileName + ".asm";
 
         try {
@@ -70,7 +66,7 @@ public class MAlice {
             buildAssembly(assemblyFileName, baseFileName);
         } catch (Exception ex) {
             die("Assembly build", ex);
-        }*/
+        }
     }
 
     private static void writeAssembly(String assemblyFileName, List<String> assembly) throws IOException {
