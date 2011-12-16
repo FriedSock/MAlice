9caccd7d75aa9dccc4ae70791e5db314e7648f24
Milan Misak <misakmilan@gmail.com>
2011-12-16 07:06:43 +0000

    Why not?

--- malice/MAlice.java
+++ malice/MAlice.java
@@ -44,7 +44,7 @@ public class MAlice {
          lexer = new MAliceLexer(fileStream);
         }
         else {
-         lexer = new MAliceLexer(new ANTLRStringStream(""));
+         lexer = new MAliceLexer(new ANTLRStringStream("\"bla \\n\" spoke."));
         }
         CommonTokenStream tokenStream = new CommonTokenStream(lexer);
         ParseTreeBuilder builder = new ParseTreeBuilder("prog");
